731ee1b5fe07bf2f04271f11fe3ef8b7a420eae3
IlianaRodriguez <ilyrc988@gmail.com>
2021-10-27 14:37:36 -0600

    Insertando pantalla principal

--- app/src/main/java/com/example/controldenotas/MainActivity.java
+++ app/src/main/java/com/example/controldenotas/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.controldenotas;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //VARIABLES
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*-------------------HOOKS-------------------------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        /*-------------------TOOL BAR-------------------------*/
+        setSupportActionBar(toolbar);
+
+        /*-------------------NAVIGATION DRAWE MENU-------------------------*/
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.navigation_drawer_close, R.string.navigation_drawer_open);drawerLayout.addDrawerListener(toggle);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+
+        }
+    }
+
 }
