f96bb9c2e66933f535a341741d92e1ca520dc155
IlianaRodriguez <ilyrc988@gmail.com>
2021-11-09 22:09:15 -0600

    correcciones del anterior COMMIT

---
+++ app/src/main/java/com/example/controldenotas/Buscando.java
@@ -0,0 +1,14 @@
+package com.example.controldenotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buscando extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscando);
+    }
+}
---
+++ app/src/main/java/com/example/controldenotas/ListaNotas.java
@@ -0,0 +1,30 @@
+package com.example.controldenotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaNotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
