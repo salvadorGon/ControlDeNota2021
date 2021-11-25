639183e294645b486286fcb4846822dedd9b396b
IngGerson <ing.gerson.gutierrez@gmail.com>
2021-11-24 23:37:13 -0600

    mostrando info en los autocompletetext parte 1

--- app/src/main/java/com/example/controldenotas/Buscando.java
+++ app/src/main/java/com/example/controldenotas/Buscando.java
@@ -1,27 +1,65 @@
 package com.example.controldenotas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+
+import com.example.controldenotas.Adaptador.ActividadAdapter;
+import com.example.controldenotas.Adaptador.AlumnoAdapter;
+import com.example.controldenotas.Dao.ActividadDao;
+import com.example.controldenotas.DaoImp.ActividadDaoImpRoom;
+import com.example.controldenotas.Models.Actividad;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Buscando extends AppCompatActivity {
 
     Button btnMostrarAlumnosCalificar;
+    AutoCompleteTextView acInstitucion, acMateria, acGrupo,acActividad;
+    private String[] universidades = { "USO", "UNAB", "UES", "UFG"};
+    List<Actividad> actividadeslist;
+    ActividadDao daoActividad;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscando);
 
-        //CARGAR DATOS A LOS
+        daoActividad=new ActividadDaoImpRoom(getApplicationContext());
 
-        //fin carga de datos
+        //CARGAR DATOS A LOS
 
         //mostrando ventana de alumnos que cumplan con los filtros
         this.btnMostrarAlumnosCalificar = (Button) findViewById(R.id.btnMostrarCALIFICAR);
+        this.acInstitucion = (AutoCompleteTextView) findViewById(R.id.acInstitucion);
+        this.acMateria = (AutoCompleteTextView) findViewById(R.id.acMateria);
+        this.acGrupo = (AutoCompleteTextView) findViewById(R.id.acGrupo);
+        this.acActividad = (AutoCompleteTextView) findViewById(R.id.acActividad);
+
         Intent intentMostrando = new Intent(this, CalificandoAlumno.class);
+        this.cargarDatos();
+
+        //adaptador de tipo string
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, universidades);
+
+        //instancia del adaptador de tipo objeto
+        ActividadAdapter adapterActividades=new ActividadAdapter(this.actividadeslist, getApplicationContext(), daoActividad);
+
+        acInstitucion.setAdapter(adapter);
+        acMateria.setAdapter(adapter);
+        acGrupo.setAdapter(adapter);
+        acActividad.setAdapter(adapter);
+        //acActividad.setAdapter(adapterActividades);
+
+        //fin carga de datos
 
         //evento
         btnMostrarAlumnosCalificar.setOnClickListener(new View.OnClickListener() {
@@ -32,5 +70,11 @@ public class Buscando extends AppCompatActivity {
         });
 
 
+
+
+    }
+    void  cargarDatos(){
+        actividadeslist=new ArrayList<Actividad>();
+        actividadeslist=daoActividad.getAll();
     }
 }
