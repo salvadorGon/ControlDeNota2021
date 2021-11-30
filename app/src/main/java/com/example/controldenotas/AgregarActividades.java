411653197f3968098a10ef713cce5d5c7db8ea22
IngGerson <ing.gerson.gutierrez@gmail.com>
2021-11-30 00:42:37 -0600

    DATOS A LAS TABLAS

--- app/src/main/java/com/example/controldenotas/AgregarActividades.java
+++ app/src/main/java/com/example/controldenotas/AgregarActividades.java
@@ -70,13 +70,38 @@ public class AgregarActividades extends AppCompatActivity {
         this.btnContinuar=(Button) findViewById(R.id.btnContinuar);
 
         //ejecutando creacion de datos
-        guardarAlumno();
-        guardarActividad();
-        guardarMateria();
+        guardarAlumno("Gerson","Otoniel", "Gutierrez", "Giron");
+        guardarAlumno("Iliana","Guadalupe", "Rodriguez", "Chica");
+        guardarAlumno("Leyssi","Rosmery", "Suriano", "Suriano");
+        guardarAlumno("Salvador","Antonio", "Hernandez", "Hernandez");
+        guardarAlumno("Keny","Guadalupe", "Lue", "Cortez");
+
+        guardarActividad("1 Examen Parcial", 20);
+        guardarActividad("2 Examen Parcial", 20);
+        guardarActividad("Laboratorio", 10);
+        guardarActividad("Practica", 10);
+        guardarActividad("Examen Final", 40);
+
+        guardarMateria("Implantacion de sistemas");
+        guardarMateria("Metodos");
+        guardarMateria("Legislacion");
+        guardarMateria("Programacion moviles");
+        guardarMateria("Arquitectura de computadoras");
+
         guardarDocente();
-        guardarGrupo();
-        guardarInstitucion();
-        guardarActividadMateriagrupo();
+
+        guardarGrupo("Grupo 1", 1);
+
+
+        guardarInstitucion("Universidad de Sonsonate");
+        guardarInstitucion("Universidad Andres Bello");
+        guardarInstitucion("Universidad Modular Abierta");
+
+        for(int i=1; i<=5;i++){
+            for(int j=1; j<=5;j++){
+                guardarActividadMateriagrupo(i, j, 1);
+            }
+        }
 
         Intent intent =new Intent(this,Principal.class);
 
@@ -92,21 +117,21 @@ public class AgregarActividades extends AppCompatActivity {
         });
 
     }
-    void  guardarActividad(){
+    void  guardarActividad(String name, int porcentaje){
         actividad=new Actividad();
-        actividad.setNombre("actividad evaluada");
-        actividad.setDescripcion("esta es una actividad evaluada");
-        actividad.setPorcentaje(10.00);
+        actividad.setNombre(name);
+        actividad.setDescripcion("Hola soy una actividad");
+        actividad.setPorcentaje(porcentaje);
         dao.save(actividad);
 
     }
 
-    void  guardarAlumno(){
+    void  guardarAlumno(String unon, String dosn, String unoa, String dosa){
         alumno=new Alumno();
-        alumno.setPrimerNombre("Gerson");
-        alumno.setSegundoNombre("Otoniel");
-        alumno.setPrimerApellido("Gutierrez");
-        alumno.setSegundoApellido("Giron");
+        alumno.setPrimerNombre(unon);
+        alumno.setSegundoNombre(dosn);
+        alumno.setPrimerApellido(unoa);
+        alumno.setSegundoApellido(dosa);
         daoalumno.save(alumno);
     }
 
@@ -119,34 +144,34 @@ public class AgregarActividades extends AppCompatActivity {
         daodocente.save(docente);
     }
 
-    void guardarGrupo(){
+    void guardarGrupo(String name, int id){
         grupo = new Grupo();
-        grupo.setNombre("GrupoAlumnos");
+        grupo.setNombre(name);
         grupo.setAnio(2021);
-        grupo.setIdMateria(1);
+        grupo.setIdMateria(id);
         daogrupo.save(grupo);
     }
 
-    void guardarInstitucion(){
+    void guardarInstitucion(String name){
         institucion = new Institucion();
-        institucion.setNombre("Universidad de Sonsonate");
+        institucion.setNombre(name);
         institucion.setTelefono("23456786");
         institucion.setIdDocente(1);
         daoinstitucion.save(institucion);
     }
 
-    void guardarMateria(){
+    void guardarMateria(String name){
         materia = new Materia();
-        materia.setNombre("Matematica");
-        materia.setDescripcion("Es una materia");
+        materia.setNombre(name);
+        materia.setDescripcion("Hola soy materia");
         daomateria.save(materia);
     }
 
-    void guardarActividadMateriagrupo(){
+    void guardarActividadMateriagrupo(int id, int id2, int id3){
         amg = new ActividadMateriaGrupo();
-        amg.setIdActividad(1);
-        amg.setIdMateria(1);
-        amg.setIdGrupo(1);
+        amg.setIdActividad(id);
+        amg.setIdMateria(id2);
+        amg.setIdGrupo(id3);
         amgdao.save(amg);
     }
 
