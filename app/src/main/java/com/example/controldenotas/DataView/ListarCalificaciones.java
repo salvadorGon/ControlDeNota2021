02af45ca5e72621d0d695f0c3cbc25d6d2fec2af
SalvadorGonzalez <89713938+salvadorGon@users.noreply.github.com>
2021-11-30 21:28:39 -0600

    Vistas Lista Actividades

---
+++ app/src/main/java/com/example/controldenotas/DataView/ListarCalificaciones.java
@@ -0,0 +1,16 @@
+package com.example.controldenotas.DataView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.controldenotas.R;
+
+public class ListarCalificaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_calificaciones);
+    }
+}
